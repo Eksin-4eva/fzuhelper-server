657860c1ec7e2de5594c3e0a61c9c164b85ce922
Xue Rui <setsunaomoi@163.com>
2026-08-18 20:08:22 +0800

    fix: remove gorm

--- pkg/db/model/course.go
+++ pkg/db/model/course.go
@@ -71,25 +71,25 @@ type AutoAdjustCourse struct {
 
 // UserCustomCourse 用户自定义课程表
 type UserCustomCourse struct {
-	ID         int64          `gorm:"column:id;primaryKey;autoIncrement"`
-	StuId      string         `gorm:"column:stu_id;size:50;not null"`
-	Term       string         `gorm:"column:term;size:20;not null"`
-	CourseId   string         `gorm:"column:course_id;size:64;not null"`
-	Name       string         `gorm:"column:name;size:100;not null"`
-	Teacher    string         `gorm:"column:teacher;size:50"`
-	Location   string         `gorm:"column:location;size:100;not null"`
-	StartClass int            `gorm:"column:start_class;not null"`
-	EndClass   int            `gorm:"column:end_class;not null"`
-	StartWeek  int            `gorm:"column:start_week;not null"`
-	EndWeek    int            `gorm:"column:end_week;not null"`
-	Weekday    int            `gorm:"column:weekday;not null"`
-	IsSingle   bool           `gorm:"column:is_single;default:false"`
-	IsDouble   bool           `gorm:"column:is_double;default:false"`
-	Color      string         `gorm:"column:color;size:20;default:#FF5733"`
-	Remark     string         `gorm:"column:remark;size:200"`
-	CreatedAt  time.Time      `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt  time.Time      `gorm:"column:updated_at;autoUpdateTime"`
-	DeletedAt  gorm.DeletedAt `gorm:"column:deleted_at"`
+	ID         int64
+	StuId      string
+	Term       string
+	CourseId   string
+	Name       string
+	Teacher    string
+	Location   string
+	StartClass int
+	EndClass   int
+	StartWeek  int
+	EndWeek    int
+	Weekday    int
+	IsSingle   bool
+	IsDouble   bool
+	Color      string
+	Remark     string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `sql:"index"`
 }
 
 // TableName 指定表名
